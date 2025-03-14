ec18aa5180a7ddec484e901889a4ea07cb8b3925
Saleh <s.hmoghaddam2005@gmail.com>
2025-03-14 14:10:53 +0330

    create sql

--- CShot.py
+++ CShot.py
@@ -9,4 +9,36 @@ class SaveData():
         self.database = database
         self.conn = None
         self.cursor = None
-        
+    def connectToDatabase(self):
+        self.conn = mysql.connector.connect(
+            host = self.host,
+            user = self.user,
+            password = self.password,
+            database=self.database
+        )
+        self.cursor = self.conn.cursor()
+    def Table(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS rank (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(50) NOT NULL,
+                point INT NOT NULL
+        )
+        ''')
+    def Register(self, name, point=0):
+        self.cursor.execute('''
+        INSERT INTO rank (name, point) VALUES (%s, %s)
+        ''', (name, point))
+        self.conn.commit()
+
+
+
+
+if __name__ == "__main__":
+    db = SaveData()
+    db.connectToDatabase()
+    db.Table()
+    db.Register("Ali", 25)
+    # db.close_connection()
+
+    
---
+++ rank player.sql
@@ -0,0 +1,5 @@
+create table rank_player(
+	player_id bigint primary key,
+		name varcharacter(50) NOT null,
+        point bigint not null
+        )
