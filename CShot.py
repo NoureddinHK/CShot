d7fc8c42ff3b36c72a6a3fbcb7a3a94026703a84
Saleh <s.hmoghaddam2005@gmail.com>
2025-03-14 11:12:05 +0330

    Database initialization

--- CShot.py
+++ CShot.py
@@ -1,1 +1,12 @@
-#test
+import mysql.connector
+
+
+class SaveData():
+    def __init__(self, host = "127.0.0.1", user = "root", password = 123456789, database = "Project_Ap"):
+        self.host = host 
+        self.user = user
+        self.password = password
+        self.database = database
+        self.conn = None
+        self.cursor = None
+        
